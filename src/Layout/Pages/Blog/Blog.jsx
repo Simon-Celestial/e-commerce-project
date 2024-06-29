25c90bf21dcda40931a76a7efc2860053a83bb63
Gasimli Ziya <gasimli.ziya@yandex.com>
2024-06-29 18:52:35 +0400

    changed old styles and old data on blog page

--- src/Layout/Pages/Blog/Blog.jsx
+++ src/Layout/Pages/Blog/Blog.jsx
@@ -1,20 +1,21 @@
-import React, { useCallback, useEffect, useMemo, useState } from 'react'
+import {useCallback, useEffect, useMemo, useState} from 'react'
 import styles from "./Blog.module.scss";
 import Header from '../../Components/Header/Header';
 import Footer from '../../Components/Footer/Footer';
 import PageHeading from '../../Common/PageHeading/PageHeading';
-import { useTranslation } from "react-i18next";
-import commentsMultiLang from "/public/data/CommentsData/commentsData.json"
-import postsMultiLang from "/public/data/PopularPostsData/popularPosts.json"
-
+import {useTranslation} from "react-i18next";
+import commentsMultiLang from "/public/data/CommentsData/commentsData.json";
+import postsMultiLang from "/public/data/PopularPostsData/popularPosts.json";
+import blogDataMultiLang from "/public/data/BlogData/blogData.json";
 
 
 const Blog = () => {
     const [postsData, setPostsData] = useState(null);
     const [postSearch, setPostSearch] = useState("");
     const [commentsData, setCommentsData] = useState(null);
+    const [blogData, setBlogData] = useState(null);
 
-    const { t } = useTranslation();
+    const {t} = useTranslation();
     const {i18n} = useTranslation();
 
 
@@ -38,171 +39,113 @@ const Blog = () => {
     }, []);
 
     useEffect(() => {
-        handleTranslateData(i18n.language,postsMultiLang,setPostsData);
-        handleTranslateData(i18n.language,commentsMultiLang,setCommentsData);
-    }, [handleTranslateData, i18n.language,setCommentsData,commentsMultiLang,setPostsData,postsMultiLang]);
+        handleTranslateData(i18n.language, postsMultiLang, setPostsData);
+        handleTranslateData(i18n.language, commentsMultiLang, setCommentsData);
+        handleTranslateData(i18n.language, blogDataMultiLang,setBlogData)
+    }, [handleTranslateData, i18n.language, setCommentsData, commentsMultiLang, setPostsData, postsMultiLang,blogDataMultiLang,setBlogData]);
 
 
     return (
         <>
-            <div className={styles.blogWrapper}>
-                <Header />
-                <main className={styles.wrapperBlog}>
-                    <PageHeading title={t("main.blog.blogBlog")} />
-                    <div className={styles.blogContainer}>
-                        <div className={styles.blogLeft}>
-                            <div className={styles.blogBox}>
-                                <div className={styles.blogImage}>
-                                    <img src='https://nooni-be87.kxcdn.com/nooni-fashion/wp-content/uploads/2023/04/blog-2.jpg' alt=''></img>
-                                </div>
-                                <div className={styles.blogText}>
-                                    <p>{t("main.blog.fashionTrending")}</p>
-                                    <h1>{t("main.blog.perfectPieces")}</h1>
-                                    <p>{t("main.blog.nuncUt")}</p>
-                                    <a href=''>{t("main.blog.readMore")}</a>
+            <Header/>
+            <main className={styles.blogMain}>
+                <PageHeading title={t("main.blog.blogBlog")}/>
+                <div className={styles.blogContent}>
+                    <div className={styles.blogLeft}>
+                        {blogData?.map((blog)=> {
+                            return (
+                                <div key={blog?.id} className={styles.blogBox}>
+                                    <div className={styles.blogImage}>
+                                        <img
+                                            src={blog?.image}
+                                            alt="Blog Image"></img>
+                                    </div>
+                                    <div className={styles.blogText}>
+                                        <b>{blog?.trending.join(", ")}</b>
+                                        <h1>{blog?.name}</h1>
+                                        <p>{blog?.content}</p>
+                                        <a href=''>{t("main.blog.readMore")}</a>
 
+                                    </div>
                                 </div>
-                            </div>
-                            <div className={styles.blogBox}>
-                                <div className={styles.blogImage}>
-                                    <img src='https://nooni-be87.kxcdn.com/nooni-fashion/wp-content/uploads/2023/04/blog-14.jpg' alt=''></img>
-                                </div>
-                                <div className={styles.blogText}>
-                                    <p>{t("main.blog.fashionMen")}</p>
-                                    <h1>{t("main.blog.theDosAnd")}</h1>
-                                    <p>{t("main.blog.nuncUt")}</p>
-                                    <a href=''>{t("main.blog.readMore")}</a>
 
-                                </div>
-                            </div>
-                            <div className={styles.blogBox}>
-                                <div className={styles.blogImage}>
-                                    <img src='https://nooni-be87.kxcdn.com/nooni-fashion/wp-content/uploads/2023/04/blog-6.jpg' alt=''></img>
-                                </div>
-                                <div className={styles.blogText}>
-                                    <p>{t("main.blog.fashionTrending")}</p>
-                                    <h1>{t("main.blog.weekdayOutfit")}</h1>
-                                    <p>{t("main.blog.nuncUt")}</p>
-                                    <a href=''>{t("main.blog.readMore")}</a>
-
-                                </div>
-                            </div>
-                            <div className={styles.blogBox}>
-                                <div className={styles.blogImage}>
-                                    <img src='https://nooni-be87.kxcdn.com/nooni-fashion/wp-content/uploads/2023/04/blog-1.jpg' alt=''></img>
-                                </div>
-                                <div className={styles.blogText}>
-                                    <p>{t("main.blog.fashionWomen")}</p>
-                                    <h1>{t("main.blog.yourSummer")}</h1>
-                                    <p>{t("main.blog.nuncUt")}</p>
-                                    <a href=''>{t("main.blog.readMore")}</a>
-
-                                </div>
-                            </div>
-                        </div>
-                        <div className={styles.blogRight}>
-                            <div className={styles.blogContact}>
-                                <div className={styles.blogMinibox}>
-                                    <div className={styles.blogInput}>
-                                        <input 
+                            )
+                        })}
+                    </div>
+                    <div className={styles.blogRight}>
+                        <div className={styles.blogContact}>
+                            <div className={styles.blogMiniBox}>
+                                <div className={styles.blogInput}>
+                                    <input
                                         type='text'
-                                            placeholder='Search for products...'
-                                            onChange={handlePostInput}
-                                            value={postSearch}/>
-                                    </div>
+                                        placeholder={t("main.search.searchProduct")}
+                                        onChange={handlePostInput}
+                                        value={postSearch}/>
                                 </div>
-                                <div className={styles.blogMinibox}>
-                                    <div className={styles.blogPopular}>
-                                        <h3>{t("main.blog.popularPosts")}</h3>
-                                        {filteredPosts?.length > 0 ?
-                                            filteredPosts?.map((post) => {
-                                                return (
-                                                    <div key={post?.id} className={styles.popularBox}>
-                                                        <img
-                                                            src={post?.image}
-                                                            alt='Post Image'></img>
-                                                        <p>{post?.title}</p>
-                                                    </div>
-                                                )
-                                            })
-                                            :
-                                            <p style={{
-                                                fontSize: "20px",
-                                                fontWeight: "500",
-                                                marginTop: "20px"
-                                            }}>No post found...</p>
-                                        }
-                                    </div>
-                                </div>
-                                <div className={styles.blogMinibox}>
-                                    <div className={styles.blogPopular}>
-                                        <h3>{t("main.blog.recentComments")}</h3>
-                                        {commentsData?.map((data) => {
+                            </div>
+                            <div className={styles.blogMiniBox}>
+                                <div className={styles.blogPopular}>
+                                    <h3>{t("main.blog.popularPosts")}</h3>
+                                    {filteredPosts?.length > 0 ?
+                                        filteredPosts?.map((post) => {
                                             return (
-                                                <div key={data?.id} className={styles.popularBox}>
-                                                    <div className={styles.blogComments}>
-                                                        <p style={{
-                                                            fontSize: "15px"
-                                                        }}>{data?.comment}</p>
-                                                        <span>{data?.date}</span>
-                                                    </div>
+                                                <div key={post?.id} className={styles.popularBox}>
+                                                    <img
+                                                        src={post?.image}
+                                                        alt='Post Image'></img>
+                                                    <p>{post?.title}</p>
                                                 </div>
                                             )
-                                        })}
-                                    </div>
+                                        })
+                                        :
+                                        <p style={{
+                                            fontSize: "20px",
+                                            fontWeight: "500",
+                                            marginTop: "20px"
+                                        }}>No post found...</p>
+                                    }
                                 </div>
-                                <div className={styles.blogMinibox}>
-                                    <div className={styles.blogPopular}>
-                                        <h3>{t("main.blog.tagsPost")}</h3>
-                                        <div className={styles.popularBox}>
-                                            <div className={styles.blogComments}>
-                                                <div className={styles.commentsBox}>
-                                                    <button>{t("main.blog.blogDresses")}</button>
-                                                    <button>{t("main.blog.blogFashionita")}</button>
+                            </div>
+                            <div className={styles.blogMiniBox}>
+                                <div className={styles.blogPopular}>
+                                    <h3>{t("main.blog.recentComments")}</h3>
+                                    {commentsData?.map((data) => {
+                                        return (
+                                            <div key={data?.id} className={styles.popularBox}>
+                                                <div className={styles.blogComments}>
+                                                    <p style={{
+                                                        fontSize: "15px"
+                                                    }}>{data?.comment}</p>
+                                                    <span>{data?.date}</span>
                                                 </div>
                                             </div>
+                                        )
+                                    })}
+                                </div>
+                            </div>
+                            <div className={styles.blogMiniBox}>
+                                <div className={styles.blogPopular}>
+                                    <h3>{t("main.blog.tagsPost")}</h3>
+                                    <div className={styles.tagsBox}>
+                                        <span>{t("main.blog.blogDresses")}</span>
+                                        <span>{t("main.blog.blogFashionita")}</span>
+                                        <span>{t("main.blog.blogFashionTrend")}</span>
+                                        <span>{t("main.blog.blogHolidaySale")}</span>
+                                        <span>{t("main.blog.blogKids")}</span>
+                                        <span>{t("main.blog.blogMenWear")}</span>
+                                        <span>{t("main.blog.saleOff")}</span>
+                                        <span>{t("main.blog.blogTipsTricks")}</span>
+                                        <span>{t("main.blog.blogTops")}</span>
+                                        <span>{t("main.blog.blogWomenWear")}</span>
 
-                                        </div>
-                                        <div className={styles.popularBox}>
-                                            <div className={styles.blogComments}>
-                                                <div className={styles.commentsBox}>
-                                                    <button>{t("main.blog.blogFashionTrend")}</button>
-                                                    <button>{t("main.blog.blogHolidaySale")}</button>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className={styles.popularBox}>
-                                            <div className={styles.blogComments}>
-                                                <div className={styles.commentsBox}>
-                                                    <button>{t("main.blog.blogKids")}</button>
-                                                    <button>{t("main.blog.blogMenWear")}</button>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className={styles.popularBox}>
-                                            <div className={styles.blogComments}>
-                                                <div className={styles.commentsBox}>
-                                                    <button>{t("main.blog.saleOff")}</button>
-                                                    <button>{t("main.blog.blogTipsTricks")}</button>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className={styles.popularBox}>
-                                            <div className={styles.blogComments}>
-                                                <div className={styles.commentsBox}>
-                                                    <button>{t("main.blog.blogTops")}</button>
-                                                    <button>{t("main.blog.blogWomenWear")}</button>
-                                                </div>
-                                            </div>
-                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </main>
-                <Footer />
-            </div>
+                </div>
+            </main>
+            <Footer/>
         </>
     )
 }
